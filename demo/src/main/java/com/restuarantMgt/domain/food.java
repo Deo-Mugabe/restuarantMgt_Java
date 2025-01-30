65d37171308f388bc83e2471ef2b0ea51f757a98
Deo_Mugabe <79472484+Deo-Mugabe@users.noreply.github.com>
2025-01-30 13:08:48 -0600

    Update food.java

--- demo/src/main/java/com/restuarantMgt/domain/food.java
+++ demo/src/main/java/com/restuarantMgt/domain/food.java
@@ -1,4 +1,15 @@
 package com.restuarantMgt.domain;
 
 public class food {
+  private int ID 
+  private string Name
+  private float Price
+  private string FoodImage 
+  private string FoodID 
+  private int MenuID
+  private date CreatedAt
+  private date UpdatedAt
+  private Menu Menu
+  
+  
 }
